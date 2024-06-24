68735df4864a54cdaf812d4b249f6b406b325e32
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-24 16:39:13 +1000

    Add public account and indexes to database

--- database/structure.sql
+++ database/structure.sql
@@ -12,6 +12,11 @@ CREATE TABLE users (
     username VARCHAR(32) NOT NULL,
     password_hash TEXT NOT NULL,
     balance BIGINT DEFAULT 0 NOT NULL,
+    public_account BOOLEAN DEFAULT TRUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+-- Add indexes to the users table
+
+CREATE INDEX users_id_idx ON users(id);
+CREATE INDEX user_username_idx ON users(username);
