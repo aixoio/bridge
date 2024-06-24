40fc4ecc14816211a4defa2714e9963cf97eb202
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-24 21:01:12 +1000

    Deleted files: .gitignore, .tmp.env, connect.go, signup.go, data.go, env.go, hasher.go, router.go; modified go.mod, go.sum, main.go.

--- server/go.mod
+++ server/go.mod
@@ -1,9 +1,3 @@
 module github.com/aixoio/bridge/server
 
 go 1.22.2
-
-require github.com/joho/godotenv v1.5.1
-
-require github.com/lib/pq v1.10.9
-
-require golang.org/x/crypto v0.24.0
--- server/main.go
+++ server/main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"github.com/aixoio/bridge/server/db"
-	"github.com/aixoio/bridge/server/env"
-	"github.com/aixoio/bridge/server/router"
-)
-
 func main() {
-	dat, err := env.LoadENV()
-	if err != nil {
-		log.Panicln("Cannot load env")
-	}
-
-	err = db.Connect(dat)
-	if err != nil {
-		log.Panicln("Cannot connect to PostgreSQL")
-	}
-	defer db.Disconnect()
-
-	router.StartServer(dat)
 
 }
