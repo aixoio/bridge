841503f9f18a290cb7e39069eac590bc8069ac90
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-24 21:07:56 +1000

    Update data.go file in the server/data directory.

--- server/data/data.go
+++ server/data/data.go
@@ -1,1 +1,12 @@
 package data
+
+import "time"
+
+type User struct {
+	Id             int
+	Username       string
+	Password_hash  string
+	Balance        int64
+	Public_Account bool
+	Created_At     time.Time
+}
