f38237386f624de9beb17f68cb56efc2aaeaca07
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-27 18:00:43 +1000

    Updated validation test for server/data/validation/validation_test.go.

--- server/data/validation/validation_test.go
+++ server/data/validation/validation_test.go
@@ -6,13 +6,13 @@ import (
 	"github.com/aixoio/bridge/server/data/validation"
 )
 
-func UsernameIsValid_Valid_Test(t *testing.T) {
+func TestUsernameIsValidValid(t *testing.T) {
 	if !validation.UsernameIsValid("a-valid-username-1234567890_ASDFGHJKLasdfghjkl") {
 		t.Fail()
 	}
 }
 
-func UsernameIsValid_Invalid_Test(t *testing.T) {
+func TestUsernameIsValidInvalid(t *testing.T) {
 	if !validation.UsernameIsValid("a-invalid-username-~!@#$%^&*()_+`-=[]\\;'\",./<>?") {
 		t.Fail()
 	}
