ac3f2cc26a0a34cc95e6ef9639bc48a117b056eb
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-27 19:00:05 +1000

    Updated validation logic in server/data/validation/validation.go.

--- server/data/validation/validation.go
+++ server/data/validation/validation.go
@@ -5,3 +5,7 @@ import "regexp"
 func UsernameIsValid(username string) bool {
 	return regexp.MustCompile("^[A-Za-z0-9_-]+$").MatchString(username)
 }
+
+func TransactionValueIsValid(value int64) bool {
+	return value > 0
+}
